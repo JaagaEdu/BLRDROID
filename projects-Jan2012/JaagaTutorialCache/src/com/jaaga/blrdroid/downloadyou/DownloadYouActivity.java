aefc88092140c4fd4ca3e3bedafb01e696b4f9ff
unknown <jaaga@jaaga-acer.(none)>
2012-02-05 18:46:31 +0530

    created an independent downloader class and list view

--- projects-Jan2012/JaagaTutorialCache/src/com/jaaga/blrdroid/downloadyou/DownloadYouActivity.java
+++ projects-Jan2012/JaagaTutorialCache/src/com/jaaga/blrdroid/downloadyou/DownloadYouActivity.java
@@ -11,137 +11,75 @@ import java.net.URL;
 import java.util.Enumeration;
 import java.util.Vector;
 
+
+
+
+
+
+
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class DownloadYouActivity extends Activity {
-
-    private class ReceivingDataFromYoutube extends AsyncTask<String, Void, Void> {
-
-        private ProgressDialog dialog = new ProgressDialog(DownloadYouActivity.this);
-        private String result;
-
-        protected void onPreExecute() {
-            dialog.setMessage("Downloading...");
-            dialog.show();
-        }
-
-        @Override
-        protected Void doInBackground(String... arg0) {
-            int begin, end;
-            String tmpstr = null;
-            try {//http://www.youtube.com/watch?v=y12-1miZHLs&nomobile=1
-            	 //http://www.youtube.com/watch?v=P_JVZgdEIJY&nomobile=1
-                URL url=new URL("http://www.youtube.com/watch?v=y12-1miZHLs&nomobile=1");
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                InputStream stream=con.getInputStream();
-                InputStreamReader reader=new InputStreamReader(stream);
-                StringBuffer buffer=new StringBuffer();
-                char[] buf=new char[262144];                
-                int chars_read;
-                while ((chars_read = reader.read(buf, 0, 262144)) != -1) {
-                    buffer.append(buf, 0, chars_read);
-                }
-                tmpstr=buffer.toString();
-
-                begin  = tmpstr.indexOf("url_encoded_fmt_stream_map=");
-                end = tmpstr.indexOf("&", begin + 27);
-                if (end == -1) {
-                    end = tmpstr.indexOf("\"", begin + 27);
-                }
-                tmpstr = URLDecoder.decode(tmpstr.substring(begin + 27, end));
-
-            } catch (MalformedURLException e) {
-                throw new RuntimeException();
-            } catch (IOException e) {
-                throw new RuntimeException();
-            }
-
-            Vector url_encoded_fmt_stream_map = new Vector();
-            begin = 0;
-            end   = tmpstr.indexOf(",");
-
-            while (end != -1) {
-                url_encoded_fmt_stream_map.addElement(tmpstr.substring(begin, end));
-                begin = end + 1;
-                end   = tmpstr.indexOf(",", begin);
-            }
-
-            url_encoded_fmt_stream_map.addElement(tmpstr.substring(begin, tmpstr.length()));
-            String result = "";
-            Enumeration url_encoded_fmt_stream_map_enum = url_encoded_fmt_stream_map.elements();
-            while (url_encoded_fmt_stream_map_enum.hasMoreElements()) {
-                tmpstr = (String)url_encoded_fmt_stream_map_enum.nextElement();
-                begin = tmpstr.indexOf("itag=");
-                if (begin != -1) {
-                    end = tmpstr.indexOf("&", begin + 5);
-
-                    if (end == -1) {
-                          end = tmpstr.length();
-                    }
-
-                    int fmt = Integer.parseInt(tmpstr.substring(begin + 5, end));
-
-                    if (fmt == 35) {
-                        begin = tmpstr.indexOf("url=");
-                        if (begin != -1) {
-                            end = tmpstr.indexOf("&", begin + 4);
-                            if (end == -1) {
-                               end = tmpstr.length();
-                            }
-                            result = URLDecoder.decode(tmpstr.substring(begin + 4, end));
-                            this.result=result;
-                            break;
-                        }
-                    }
-                }
-            }         
-            try {
-              URL u = new URL(result);
-              HttpURLConnection c = (HttpURLConnection) u.openConnection();
-              c.setRequestMethod("GET");
-/*              c.setRequestProperty("Youtubedl-no-compression", "True");
-              c.setRequestProperty("User-Agent", "YouTube");*/
-
-              c.setDoOutput(true);
-              c.connect();
-
-              FileOutputStream f=new FileOutputStream(new File("/sdcard/new.flv"));
-
-              InputStream in=c.getInputStream();
-              byte[] buffer=new byte[1024];
-              int sz = 0;
-              while ( (sz = in.read(buffer)) > 0 ) {
-                   f.write(buffer,0, sz);
-              }
-              f.close();
-            } catch (MalformedURLException e) {
-                new RuntimeException();
-            } catch (IOException e) {
-                new RuntimeException();
-            }
-            return null;
-        }
-
-        protected void onPostExecute(Void unused) {
-            dialog.dismiss();
-        }    
-
-    }
-
-
+	// Layout
+	View listView; // view
+	LinearLayout layout;
+	LayoutInflater layoutInflater;
+	ListView displayView;
+	String array[] = null;
+	ArrayAdapter<String> adapter = null;
+	String tag =  "DownloadYouActivity";
+	public static int listView_rid = R.layout.listview;// resource id
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        layout = (LinearLayout)findViewById(R.id.mainLayout);
+		layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		listView = layoutInflater.inflate(listView_rid, null); // state 2
+		
+		displayView = (ListView) listView.findViewById(R.id.list01);
 
-        new ReceivingDataFromYoutube().execute();
+		layout.addView(displayView);
+		
+		update();
+		
+		
+        //new Downloader(this).execute();
     }   
+    
+    public void update() {
+		Log.i(tag, "Update!");
+		// super.update();
+		
+			String path = "/sdcard";
+			File fil = new File(path);
+			if (fil.isDirectory()) {
+
+				array = fil.list();
+				for (int i = 0; i < array.length; i++) {
+
+				}
+				adapter = new ArrayAdapter<String>(this,
+						android.R.layout.simple_list_item_1, array);
+
+			}
+
+			displayView.setAdapter(adapter);
+		
+
+	}
+
 }  
